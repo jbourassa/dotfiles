b62b9ed9f8bbbdcea49d07370acbd1d0d9103b4a
Jimmy Bourassa <jimmy.bourassa@hooktstudios.com>
2012-10-05 12:41:38 -0400

    Add some osx defaults

---
+++ osx.sh
@@ -0,0 +1,17 @@
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Repeat delay
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+
+# faster key repeat
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+# No shadow in screens
+defaults write com.apple.screencapture disable-shadow -bool true

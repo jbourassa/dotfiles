7e3c0ebcc8754e232fb86a63d291869f2a39f422
Jimmy Bourassa <jbourassa@gmail.com>
2018-05-15 00:42:24 -0400

    Update configs to my current likings

--- osx.sh
+++ osx.sh
@@ -8,12 +8,42 @@ defaults write com.apple.finder QLEnableTextSelection -bool true
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 # Repeat delay
-defaults write NSGlobalDomain InitialKeyRepeat -int 12
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # faster key repeat
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+# Disable Slow Keys.
+defaults write com.apple.universalaccess slowKey -int 0
+
+# Disable Press-And-Hold for keys (eg: accents)
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Disable “natural” (Lion-style) scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
 # No shadow in screens
 defaults write com.apple.screencapture disable-shadow -bool true
 
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+
+# My hot corners – tl:appwindowd tr:desktop br:allwindows
+defaults write com.apple.Dock wvous-tr-corner -int 4;
+defaults write com.apple.Dock wvous-br-corner -int 2;
+defaults write com.apple.Dock wvous-tl-corner -int 3;
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Disable smart subs for code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
--- vim/vim.symlink/after/plugin/abolish.vim
+++ vim/vim.symlink/after/plugin/abolish.vim
@@ -1,1 +1,2 @@
 Abolish temp{al}te{,s} temp{la}te{,s}
+Abolish co{m}itter co{mm}itter

f50b0b7634d6f374e1293e5c123b910894a0c9fa
Jimmy Bourassa <jbourassa@gmail.com>
2019-01-30 09:19:59 -0500

    Update misc configs

---
+++ brew.sh
@@ -0,0 +1,4 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install postgres postgis ag ack 
+brew cask install keepingyouawake iterm2 macvim 1password rowanj-gitx vagrant chefdk vlc licecap sequel-pro

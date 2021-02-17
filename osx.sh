7409f5bc232c8ebb39e4fe3b64bc3871b82c0693
Jimmy Bourassa <jbourassa@gmail.com>
2021-02-16 19:56:01 -0500

    Updates post-new-computer-setup

--- osx.sh
+++ osx.sh
@@ -1,3 +1,4 @@
+sudo -v
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
@@ -13,8 +14,11 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 12
 # faster key repeat
 defaults write NSGlobalDomain KeyRepeat -int 2
 
-# Disable Slow Keys.
-defaults write com.apple.universalaccess slowKey -int 0
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable Slow Keys. -- this fails and I don't really understand what it does?
+# defaults write com.apple.universalaccess slowKey -int 0
 
 # Disable Press-And-Hold for keys (eg: accents)
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
@@ -22,10 +26,13 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 # Disable “natural” (Lion-style) scrolling
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
+# Disable Force click with trackpad (I always hit it by accident)
+defaults write NSGlobalDomain com.apple.trackpad.forceClick -bool false
+
 # No shadow in screens
 defaults write com.apple.screencapture disable-shadow -bool true
 
-defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+sudo defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
 
 # My hot corners – tl:appwindowd tr:desktop br:allwindows
 defaults write com.apple.Dock wvous-tr-corner -int 4;
@@ -49,3 +56,6 @@ defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
 defaults write -g NSWindowShouldDragOnGesture -bool true
+
+# Swiping doesn't back/forward in browser
+defaults write .GlobalPreferences AppleEnableSwipeNavigateWithScrolls 0

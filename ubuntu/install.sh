a43b6c98eb0bb10121f71f2f7a523858a0578d2b
Jimmy Bourassa <jbourassa@gmail.com>
2021-06-29 16:16:16 -0400

    Update

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,3 +1,4 @@
-sudo apt-get install -y ripgrep mycli neovim bat htop
+sudo apt install -y -o Dpkg::Options::="--force-overwrite" ripgrep
+sudo apt-get install -y mycli neovim htop
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --bin

a43338c57ed75d49cc199c40307ab781b43ae737
Jimmy Bourassa <jbourassa@gmail.com>
2021-08-03 09:13:49 -0400

    Fix spin setup

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,4 +1,4 @@
-sudo apt install -y -o Dpkg::Options::="--force-overwrite" ripgrep
-sudo apt-get install -y mycli neovim htop
+sudo apt-get install -y mycli neovim htop ripgrep
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --bin
+sudo ln -s ~/.fzf/bin/fzf /usr/local/bin/fzf

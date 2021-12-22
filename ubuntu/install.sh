0a272e44a7c1ac75038f8ffe163b355211d8f530
Jimmy Bourassa <jbourassa@gmail.com>
2021-12-22 11:15:02 -0500

    Use latest neovim on ubuntu, from unstable channel

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,3 +1,4 @@
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt-get install -y mycli neovim htop ripgrep
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --bin

72ab2b1377dd4a5462a29acbb60f7402c4964d63
Jimmy Bourassa <jimmy.bourassa@shopify.com>
2024-02-19 11:14:03 -0500

    Install lazygit on Ubuntu

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -3,3 +3,9 @@ sudo apt-get install -y mycli neovim htop ripgrep
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --bin
 sudo ln -s ~/.fzf/bin/fzf /usr/local/bin/fzf
+
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
